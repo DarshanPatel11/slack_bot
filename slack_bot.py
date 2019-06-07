b3bdec2ce07eef2e837f672a4b43eac9962204be
Darshan Patel <djsmarty99@gmail.com>
2019-06-07 16:31:32 +0530

    Add files via upload

--- slack_bot.py
+++ slack_bot.py
@@ -16,11 +16,13 @@ slack_client = SlackClient(os.environ['TOKEN'])
 def send_morning_msg(): 
     #slack_client.api_call("chat.postMessage",channel="#test",text="What are you planning to accomplish today?")
     print("Morning Msg Sent...")
+    time.sleep(100)
 
 
 def send_evening_msg(): 
     #slack_client.api_call("chat.postMessage",channel="#test",text="So, What did you achieved today?")
     print("Evening Msg Sent...")
+    time.sleep(100)
 
 
 '''
@@ -62,7 +64,6 @@ while True:
         send_evening_msg()
     elif dt.hour == 10 and dt.minute == 15:
         send_morning_msg()
-    elif dt.hour == 16 and dt.minute == 30:
+    elif dt.hour == 16 and dt.minute == 33:
         print("hello")
-    else:
-        time.sleep(80)
+        time.sleep(100)
