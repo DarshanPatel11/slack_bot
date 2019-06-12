cf111901ea3706c0e4018f56fc60be73fa265029
Darshan Patel <djsmarty99@gmail.com>
2019-06-12 12:32:30 +0530

    Update slack_bot.py

--- slack_bot.py
+++ slack_bot.py
@@ -13,54 +13,44 @@ slack_client = SlackClient(os.environ['TOKEN'])
 # starterbot's user ID in Slack: value is assigned after the bot starts up
 
 
-def send_morning_msg(): 
-    slack_client.api_call("chat.postMessage",channel="#test",text="What are you planning to accomplish today?")
-    print("Morning Msg Sent...")
+def send_morning_msg_to_id(id): 
+    slack_client.api_call("chat.postMessage",channel=id,text="What are you planning to accomplish today? Report it to Mr. Mitul Jain")
+    #print("Morning Msg Sent...")
+    #time.sleep(100)
+
+
+def send_evening_msg_to_id(id): 
+    slack_client.api_call("chat.postMessage",channel=id,text="So, What did you achieved today? Report it to Mr. Mitul Jain")
+    #print("Evening Msg Sent...")
+    #time.sleep(100)
+
+def send_evening_msg(uids):
+    for id in uids:
+        send_evening_msg_to_id(id['id'])
+    print("Evening Msg sent...")
     time.sleep(100)
-
-
-def send_evening_msg(): 
-    slack_client.api_call("chat.postMessage",channel="#test",text="So, What did you achieved today?")
-    print("Evening Msg Sent...")
+    
+def send_morning_msg(uids):
+    for id in uids:
+        send_morning_msg_to_id(id['id'])
+    print("Morning Msg sent...")
     time.sleep(100)
-
-
-'''
-# Task scheduling 
-# After every 10mins geeks() is called.  
-#schedule.every(1).minutes.do(send_msg) 
-schedule.every().monday.at("10:15").do(send_morning_msg)
-schedule.every().monday.at("18:25").do(send_morning_msg)    
-
-schedule.every().tuesday.at("10:15").do(send_morning_msg)
-schedule.every().tuesday.at("18:25").do(send_morning_msg)
-
-schedule.every().wednesday.at("10:15").do(send_morning_msg)
-schedule.every().wednesday.at("18:25").do(send_morning_msg)
-
-schedule.every().thursday.at("10:15").do(send_morning_msg)
-schedule.every().thursday.at("18:25").do(send_morning_msg)
-
-schedule.every().friday.at("10:15").do(send_morning_msg)
-schedule.every().friday.at("18:25").do(send_morning_msg)
-
-print("Started...")
-while True:
-    # Checks whether a scheduled task  
-    # is pending to run or not 
-    schedule.run_pending() 
-    time.sleep(1) 
-'''
-
 print('started...')
 #dt = datetime.datetime.now()
 from pytz import timezone
 #print(dt)        
+users_call = slack_client.api_call("users.list")
+uids = []
+if users_call.get('ok'):
+    uids = users_call['members']
+
 while True:
     now_utc = datetime.datetime.now(timezone('UTC'))    
     dt = now_utc.astimezone(timezone('Asia/Kolkata'))
     #dt = datetime.datetime.now()
     if dt.hour == 18 and dt.minute == 25:
-        send_evening_msg()
+        send_evening_msg(uids)
     elif dt.hour == 10 and dt.minute == 15:
-        send_morning_msg()
+        send_morning_msg(uids)
+
+#send_morning_msg(uids)
